55c0b0abc581aa860ccd17645073de3bcfdc01ae
Ekaterina Kuznetsova <eantonuk@gmail.com>
2022-04-06 17:40:27 +0300

    Added button new game

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -13,91 +13,92 @@ class ViewController: UIViewController {
     @IBOutlet var letterButtons: [UIButton]!
     @IBOutlet weak var correctWordLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var NewGame: UIButton!
     //Mark: - properties
     var currentGame: Game!
     let incorrectMovesAllowed = 7
-    var listOfWords = [
-    "Дели",
-    "Шанхай",
-    "Сан-Паулу",
-    "Мехико",
-    "Каир",
-    "Мумбаи",
-    "Пекин",
-    "Дакка",
-    "Осака",
-    "Нью-Йорк",
-    "Карачи",
-    "Буэнос-Айрес",
-    "Чунцин",
-    "Стамбул",
-    "Калькутта",
-    "Манила",
-    "Лагос",
-    "Рио-де-Жанейро",
-    "Тяньцзинь",
-    "Киншаса",
-    "Гуанчжоу",
-    "Лос-Анджелес",
-    "Москва",
-    "Шэньчжэнь",
-    "Лахор",
-    "Бангалор",
-    "Париж",
-    "Богота",
-    "Джакарта",
-    "Ченнай",
-    "Лима",
-    "Бангкок",
-    "Сеул",
-    "Нагоя",
-    "Хайдарабад",
-    "Лондон",
-    "Тегеран",
-    "Чикаго",
-    "Чэнду",
-    "Нанкин",
-    "Ухань",
-    "Хошимин",
-    "Луанда",
-    "Ахмедабад",
-    "Куала Лумпур",
-    "Сиань",
-    "Гонконг",
-    "Дунгуань",
-    "Ханчжоу",
-    "Фошань",
-    "Шэньян",
-    "Эр-Рияд",
-    "Багдад",
-    "Сантьяго",
-    "Сурат",
-    "Мадрид",
-    "Сучжоу",
-    "Пуна",
-    "Харбин",
-    "Хьюстон",
-    "Даллас",
-    "Торонто",
-    "Дар-эс-Салам",
-    "Майами",
-    "Белу-Оризонти",
-    "Сингапур",
-    "Филадельфия",
-    "Атланта",
-    "Фукуока",
-    "Хартум",
-    "Барселона",
-    "Йоханнесбург",
-    "Санкт-Петербург",
-    "Циндао",
-    "Далянь",
-    "Вашингтон",
-    "Янгон",
-    "Александрия",
-    "Цзинань",
-    "Гвадалахара"
-    ].shuffled()
+    let listOfWordsAll = [
+        "Дели",
+        "Шанхай",
+    //    "Сан-Паулу",
+    //    "Мехико",
+    //    "Каир",
+    //    "Мумбаи",
+    //    "Пекин",
+    //    "Дакка",
+    //    "Осака",
+    //    "Нью-Йорк",
+    //    "Карачи",
+    //    "Буэнос-Айрес",
+    //    "Чунцин",
+    //    "Стамбул",
+    //    "Калькутта",
+    //    "Манила",
+    //    "Лагос",
+    //    "Рио-де-Жанейро",
+    //    "Тяньцзинь",
+    //    "Киншаса",
+    //    "Гуанчжоу",
+    //    "Лос-Анджелес",
+    //    "Москва",
+    //    "Шэньчжэнь",
+    //    "Лахор",
+    //    "Бангалор",
+    //    "Париж",
+    //    "Богота",
+    //    "Джакарта",
+    //    "Ченнай",
+    //    "Лима",
+    //    "Бангкок",
+    //    "Сеул",
+    //    "Нагоя",
+    //    "Хайдарабад",
+    //    "Лондон",
+    //    "Тегеран",
+    //    "Чикаго",
+    //    "Чэнду",
+    //    "Нанкин",
+    //    "Ухань",
+    //    "Хошимин",
+    //    "Луанда",
+    //    "Ахмедабад",
+    //    "Куала Лумпур",
+    //    "Сиань",
+    //    "Гонконг",
+    //    "Дунгуань",
+    //    "Ханчжоу",
+    //    "Фошань",
+    //    "Шэньян",
+    //    "Эр-Рияд",
+    //    "Багдад",
+    //    "Сантьяго",
+    //    "Сурат",
+    //    "Мадрид",
+    //    "Сучжоу",
+    //    "Пуна",
+    //    "Харбин",
+    //    "Хьюстон",
+    //    "Даллас",
+    //    "Торонто",
+    //    "Дар-эс-Салам",
+    //    "Майами",
+    //    "Белу-Оризонти",
+    //    "Сингапур",
+    //    "Филадельфия",
+    //    "Атланта",
+    //    "Фукуока",
+    //    "Хартум",
+    //    "Барселона",
+    //    "Йоханнесбург",
+    //    "Санкт-Петербург",
+    //    "Циндао",
+    //    "Далянь",
+    //    "Вашингтон",
+    //    "Янгон",
+    //    "Александрия",
+    //    "Цзинань",
+    //    "Гвадалахара"
+        ].shuffled()
     var totalWins = 0 {
         didSet {
             newRound()
@@ -110,27 +111,36 @@ class ViewController: UIViewController {
     }
     
     //Mark: - Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        newRound()
+    }
+    
     func enableButtons(_ enable: Bool = true) {
         for button in letterButtons {
             button.isEnabled = enable
         }
     }
     
+    var listOfWords: [String] = []
     
     func newRound() {
-        guard !listOfWords.isEmpty else {
+        if listOfWords.isEmpty {
+            listOfWords = listOfWordsAll
             enableButtons(false)
+        }
+        guard !listOfWords.isEmpty else {
+            enableButtons()
             updateUI()
             return
         }
+        
+
         let newWord = listOfWords.removeFirst()
         currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
         updateUI()
         enableButtons()
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        newRound()
     }
     
     func updateCorrectWordLabel() {
@@ -157,8 +167,20 @@ class ViewController: UIViewController {
         let image = "Tree\(imageNumber)"
         treeImageView.image = UIImage(named: image)
         updateCorrectWordLabel()
-        scoreLabel.text = "Выйгрыши: \(totalWins), проигрыши: \(totalLosses)"
+        scoreLabel.text = "Выйгрыши: \(totalWins), Проигрыши: \(totalLosses)"
+        scoreLabel.textColor = .red
     }
+    func setup() {
+       
+        enableButtons()
+        totalWins = 0
+        totalLosses = 0
+        newRound()
+        updateUI()
+       
+    }
+    
+    
 // Mark: - IB Actions
 
     @IBAction func letterButtonPressed(_ sender: UIButton) {
@@ -167,5 +189,13 @@ class ViewController: UIViewController {
         currentGame.playerGuessed(letter: Character(letter))
         updateState()
     }
+    
+    @IBAction func newGamePressed(_ sender: UIButton) {
+        setup()
+
+        
+      
+    }
+   
 }
 
